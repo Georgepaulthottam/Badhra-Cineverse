cdd6dea07038e07b724ed7a6534587acaff4f637
Alwin Joseph <alwinjoseph8086@gmail.com>
2023-08-01 16:25:11 +0530

    added logout to all pages

--- Attendance.php
+++ Attendance.php
@@ -242,7 +242,7 @@ header('location:Attendance.php');
 														<span class="material-icons">settings</span>
 														Settings
 													</a></li>
-												<li><a href="#">
+												<li><a href="logout.php">
 														<span class="material-icons">logout</span>
 														Logout
 													</a></li>
