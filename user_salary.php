8b3facc79eaddb55a585612d0c81193fe3b440b0
Joel <jjoel1478@gmail.com>
2023-08-09 12:31:51 +0530

    calenadr

--- user_salary.php
+++ user_salary.php
@@ -1,3 +1,17 @@
+<?php  
+session_start();
+// Check if the user is not logged in
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+
+}
+require 'connection.php';
+$user=$_SESSION['user'];
+$query=("SELECT * FROM cart WHERE username='".mysqli_real_escape_string($conn,$user)."'");
+$result=mysqli_query($conn,$query);
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -6,29 +20,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <title>Art Department</title>
+    <title>Admin Dashboard</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- css3 -->
+    <!----css3---->
     <link rel="stylesheet" href="css/custom.css">
+
 
     <!--google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 
+
     <!--google material icon-->
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css" />
-
-    <!--user css-->
-    <link rel="stylesheet" href="css/user.css" />
-
     <script type="text/javascript" src="main.js"></script>
-
-    <!-- Include Chart.js library -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
@@ -47,16 +56,16 @@
 			<h3><span  > &nbsp;Badhra Cineverse</span></h3>
             </div>
             <ul class="list-unstyled component m-0">
-                <li class="active">
+                <li >
                     <a href="#" class="dashboard"><i class="material-icons">dashboard</i>Dashboard </a>
                 </li>
 
-                <li class="dropdown">
+                <li class="active">
                     <a href="#homeSubmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                         <i class="material-icons">aspect_ratio</i>Requests
                     </a>
                     <ul class="collapse list-unstyled menu" id="homeSubmenu1">
-                    <li><a href="user_view_request.php">Pending Requests</a></li>
+                    <li class="active"><a href="user_view_request.php">Pending Requests</a></li>
 			
                     </ul>
                 </li>
@@ -144,6 +153,11 @@
 
 
 
+
+        <!-------sidebar--design- close----------->
+
+
+
         <!-------page-content start----------->
 
         <div id="content">
@@ -160,7 +174,17 @@
                         </div>
 
                         <div class="col-md-5 col-lg-3 order-3 order-md-2">
-                            
+                            <div class="xp-searchbar">
+                                <form>
+                                    <div class="input-group">
+                                        <input type="search" class="form-control" placeholder="Search">
+                                        <div class="input-group-append">
+                                            <button class="btn" type="submit" id="button-addon2">Go
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
 
 
@@ -218,7 +242,7 @@
                     </div>
 
                     <div class="xp-breadcrumbbar text-center">
-                        <h4 class="page-title">Art Department</h4>
+                        <h4 class="page-title">Request Details</h4>
                         <ol class="breadcrumb">
 
                         </ol>
@@ -231,17 +255,67 @@
 
 
             <!------main-content-start----------->
-            <!------main container divided into 3 containers: top, middle,bottom----------->
-            <!------top-container contains attendence, request status and schedule----------->
-            <div id="main-container" class="middle-section">
-                <div id="calendar" style="overflow-x:auto;"></div>
-                <div class="popup" id="dateDetailsPopup">
-                <div class="popup-content" id="popupContent">
-                <!-- Salary and request details will be displayed here -->
+            <div class="main-content">
+            <div class="top-section" id="request-table">
+                        
+                        <div id="currentDateSalary" class="profile-box"  style="overflow-x:auto;">
+                        <h3 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Request
+                        </h3>
+    
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>Amount</th>
+                                        <th>DA</th>
+                                        <th>Tax</th>
+                                        <th>EA</th>
+                                        <th>Salary</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2023/08/23</td>
+                                        <td>10000</td>
+                                        <td>200</td>
+                                        <td>500</td>
+                                        <td>250</td>
+                                        <td>8000</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+    
+                        </div>
+                        <div class="detailed-box" id="request-table">
+                        <h3 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Request
+                        </h3>
+                        <div class="attendence" style="overflow-x:auto;">
+    
+                        <table class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>Amount</th>
+                                        <th>DA</th>
+                                        <th>Tax</th>
+                                        <th>EA</th>
+                                        <th>Salary</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2023/08/23</td>
+                                        <td>10000</td>
+                                        <td>200</td>
+                                        <td>500</td>
+                                        <td>250</td>
+                                        <td>8000</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+    
+                        </div>
             </div>
-    </div>
-            </div>
-
             <!------main-content-end----------->
 
 
@@ -266,239 +340,39 @@
 
 
     <!-------complete html----------->
-    
-    <script>
-        // Include your database connection file here
-        // Replace 'YOUR_DB_CONNECTION' with the actual connection code
-        <?php
-        $conn = mysqli_connect('localhost', 'root', '', 'sample') or die(mysqli_error());
 
-        // Function to fetch events from the database
-        function getEventsFromDatabase() {
-            // Replace 'calendar_events' with your actual table name
-            $query = "SELECT * FROM calendar_events";
-            $result = mysqli_query($GLOBALS['conn'], $query);
 
-            $events = array();
-            while ($row = mysqli_fetch_assoc($result)) {
-                $events[] = $row;
-            }
 
-            return $events;
-        }
 
-        // Fetch events from the database
-        $events = getEventsFromDatabase();
 
-        // Convert the PHP array to a JSON object for JavaScript usage
-        $events_json = json_encode($events);
-        ?>
-
-        // Sample data for important dates (replace this with data fetched from the database)
-        var importantDates = <?php echo $events_json; ?>;
-        var currentDate = new Date();
-
-        function attachEventListenersToDates() {
-            const calendarDates = document.querySelectorAll('.dateLink');
-            const popup = document.getElementById('dateDetailsPopup');
-            const popupContent = document.getElementById('popupContent');
-
-            calendarDates.forEach(date => {
-                date.addEventListener('click', () => {
-                    const selectedDate = date.dataset.date;
-                    console.log(selectedDate);
-                    fetchDateDetails(selectedDate);
-                });
-            });
-            function fetchDateDetails(selectedDate) {
-                // Send a request to the PHP backend to retrieve salary and request details
-                fetch(`get_date_details.php?date=${selectedDate}`)
-                    .then(response => response.json())
-                    .then(data => {
-                        if (Object.keys(data).length < 2) {
-                popupContent.innerHTML = `<p>No data available for ${selectedDate}</p>`;
-            } else {
-                // Display salary and request details in the popup
-                popupContent.innerHTML = `
-                    <h4>Details for ${selectedDate}</h4>
-                    <p>Salary: ${data.salary}</p>
-                    <p>Requests: ${data.requests}</p>
-                    <p style="color:green">Accepted: ${data.requests}</p>
-                    <p style="color:red">Rejected: ${data.requests}</p>
-                    <a style="color:red; margin-left:35%;" href="details_page.html?date=${selectedDate}">View More</a>
-                `
-            }
-                        // Show the popup
-                        popup.style.display = 'block';
-                    })
-                    .catch(error => {
-                        console.error('Error fetching date details:', error);
-                    });
-            }
-
-            popup.addEventListener('click', function (event) {
-                if (event.target === popup) {
-                    popup.style.display = 'none';
-                }
-            });
-        }
-        function createCalendar(year, month) {
-            var calendar = document.getElementById('calendar');
-            var date = new Date(year, month - 1, 1); // Note: month is 0-indexed
-            var currentMonth = date.getMonth();
-            var today = new Date();
-
-            // Clear previous calendar content
-            calendar.innerHTML = '';
-
-            // Create the table for the current month
-            var table = document.createElement('table');
-            var caption = document.createElement('caption');
-            var navrow=document.createElement('tr');
-            var navcell1=document.createElement('td');
-            var navcell2=document.createElement('td');
-            var navcell3=document.createElement('td');
-            caption.textContent = new Intl.DateTimeFormat('en-US', { month: 'long'}).format(date);
-            var prevButton = document.createElement('span');
-            prevButton.classList.add('navigation-btn');
-            prevButton.innerHTML = '&#8249;';
-            prevButton.onclick = prevMonth;
-            navcell1.appendChild(prevButton);
-            navcell2.appendChild(caption);
-            var nextButton = document.createElement('span');
-            nextButton.classList.add('navigation-btn');
-            nextButton.innerHTML = '&#8250;';
-            nextButton.onclick = nextMonth;
-            navcell3.appendChild(nextButton);
-            navcell2.colSpan="5";
-            caption.classList.add('captioncell');
-            navrow.appendChild(navcell1);
-            navrow.appendChild(navcell2);
-            navrow.appendChild(navcell3);
-            table.appendChild(navrow);
-
-            // Create the table header row with day names
-            var headerRow = document.createElement('tr');
-            ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].forEach(day => {
-                var cell = document.createElement('th');
-                cell.textContent = day;
-                headerRow.appendChild(cell);
-            });
-            table.appendChild(headerRow);
-
-            // Find the day of the week for the first day of the month
-            var firstDayOfWeek = date.getDay();
-
-            // Get the number of days in the current month
-            var daysInMonth = new Date(year, month, 0).getDate();
-
-            // Calculate the total number of cells needed in the table
-            var totalCells = Math.ceil((firstDayOfWeek + daysInMonth) / 7) * 7;
-
-            // Create the table rows for each day
-            var currentDay = 1;
-            for (var i = 0; i < totalCells; i++) {
-                if (i % 7 === 0) {
-                    var row = document.createElement('tr');
-                    table.appendChild(row);
-                }
-
-                var cell = document.createElement('td');
-
-                if (i >= firstDayOfWeek && currentDay <= daysInMonth) {
-                    var currentDate = new Date(year, month - 1, currentDay + 1).toISOString().split('T')[0];
-                    var eventsForCurrentDate = importantDates.filter(event => event.date === currentDate);
-
-                    // Create markers for important dates
-                    if (eventsForCurrentDate.length > 0) {
-                        cell.classList.add('important-date');
-                    }
-
-                    // Check if the date is in the current month
-                    if (currentMonth === today.getMonth() && currentDay === today.getDate() && year === today.getFullYear()) {
-                        cell.classList.add('current-day');
-                        cell.classList.add('current-month');
-                    }
-
-                    // Create a link for each date
-                    cell.textContent = currentDay;
-                    cell.classList.add('dateLink');
-                    cell.dataset.date = currentDate;
-                    currentDay++;
-                }
-
-                row.appendChild(cell);
-            }
-
-            // Add the table to the calendar
-            calendar.appendChild(table);
-            attachEventListenersToDates(); 
-        }
-
-        // Get the current date
-        var currentDate = new Date();
-
-        // Initial calendar
-        createCalendar(currentDate.getFullYear(), currentDate.getMonth() + 1);
-
-        // Function to navigate to previous month
-        function prevMonth() {
-            var currentMonth = currentDate.getMonth();
-            var currentYear = currentDate.getFullYear();
-
-            if (currentMonth === 0) {
-                currentDate.setFullYear(currentYear - 1);
-                currentDate.setMonth(11);
-            } else {
-                currentDate.setMonth(currentMonth - 1);
-            }
-
-            createCalendar(currentDate.getFullYear(), currentDate.getMonth() + 1);
-        }
-
-        // Function to navigate to next month
-        function nextMonth() {
-            var currentMonth = currentDate.getMonth();
-            var currentYear = currentDate.getFullYear();
-
-            if (currentMonth === 11) {
-                currentDate.setFullYear(currentYear + 1);
-                currentDate.setMonth(0);
-            } else {
-                currentDate.setMonth(currentMonth + 1);
-            }
-
-            createCalendar(currentDate.getFullYear(), currentDate.getMonth() + 1);
-        }
-
-        // Add navigation buttons to the container with 'navigation' class
-        </script>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.slim.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
 
-    <script src="js/jquery-3.3.1.slim.min.js"></script>
-   <script src="js/popper.min.js"></script>
-   <script src="js/bootstrap.min.js"></script>
-   <script src="js/jquery-3.3.1.min.js"></script>
-  
-  
-  <script type="text/javascript">
-       $(document).ready(function(){
-	      $(".xp-menubar").on('click',function(){
-		    $("#sidebar").toggleClass('active');
-			$("#content").toggleClass('active');
-		  });
-		  
-		  $('.xp-menubar,.body-overlay').on('click',function(){
-		     $("#sidebar,.body-overlay").toggleClass('show-nav');
-		  });
-		  
-	   });
-  </script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(".xp-menubar").on('click', function () {
+                $("#sidebar").toggleClass('active');
+                $("#content").toggleClass('active');
+            });
+
+            $('.xp-menubar,.body-overlay').on('click', function () {
+                $("#sidebar,.body-overlay").toggleClass('show-nav');
+            });
+
+        });
+
+    </script>
+
+
+
+
+
 </body>
 
 </html>
