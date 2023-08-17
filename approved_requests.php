08ce00da62de3ff052787b920d6d39f7041749bf
DeminKB <deminkbenny007@gmail.com>
2023-08-17 14:25:56 +0530

    headersidebar fix

--- approved_requests.php
+++ approved_requests.php
@@ -14,6 +14,7 @@ $user=$_SESSION['user'];
 
 
 ?>
+<?php $activePage = 'request'; include 'adminheadersidebar.php'; ?>
 <!doctype html>
 <html lang="en">
 
@@ -59,203 +60,7 @@ $user=$_SESSION['user'];
 
 
 
-	<div class="wrapper">
-
-		<div class="body-overlay"></div>
-
-		<!-------sidebar--design------------>
-
-		<div id="sidebar">
-			<div class="sidebar-header">
-				<img src="Bc_logo.png" width="50px" height="50px">
-		   <h3><span  > &nbsp;Badhra Cineverse</span></h3>
-			</div>
-			<ul class="list-unstyled component m-0">
-				<li class="dropdown">
-					<a href="index.php" class="dashboard"><i class="material-icons">dashboard</i>Dashboard </a>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">aspect_ratio</i>Profile
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu1">
-						<li><a href="profile.html">Profile </a></li>
-						
-					</ul>
-				</li>
-
-
-				<li class="dropdown">
-					<a href="#homeSubmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">aspect_ratio</i>Attendance
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu1">
-						<li><a href="Attendance.php">Attendance Requests</a></li>
-						<li><a href="approved_attendance.php">Accepted Attendance</a></li>
-						
-					</ul>
-				</li>
-
-				<li class="active">
-					<a href="#homeSubmenu3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">equalizer</i>Request Panel
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu3">
-						<li><a href="Requests.php">Pending Requests</a></li>
-                        <li><a href="approved_requests.php">approved Requests</a></li>
-						<li><a href="#">Dept 1</a></li>
-						<li><a href="#">Dept 2</a></li>
-					</ul>
-				</li>
-
-
-				<li class="dropdown">
-					<a href="#homeSubmenu4" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">extension</i>Crew Management
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu4">
-						<li><a href="#">Pages 1</a></li>
-						<li><a href="#">Pages 2</a></li>
-						<li><a href="#">Pages 3</a></li>
-					</ul>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu5" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">border_color</i>Salary Manager
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu5">
-						<li><a href="#">Pages 1</a></li>
-						<li><a href="#">Pages 2</a></li>
-						<li><a href="#">Pages 3</a></li>
-					</ul>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu6" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">grid_on</i>Locations
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu6">
-						<li><a href="#">table 1</a></li>
-						<li><a href="#">table 2</a></li>
-						<li><a href="#">table 3</a></li>
-					</ul>
-				</li>
-
-
-				<li class="dropdown">
-					<a href="#homeSubmenu7" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">content_copy</i>Pages
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu7">
-						<li><a href="#">Pages 1</a></li>
-						<li><a href="#">Pages 2</a></li>
-						<li><a href="#">Pages 3</a></li>
-					</ul>
-				</li>
-
-
-				<li class="">
-					<a href="#" class=""><i class="material-icons">date_range</i>copy </a>
-				</li>
-				<li class="">
-					<a href="#" class=""><i class="material-icons">library_books</i>calender </a>
-				</li>
-				<li class="">
-					<a href="expensereport.php" class=""><i class="material-icons">currency_rupee</i>Expense Report</a>
-				</li>
-
-			</ul>
-		</div>
-
-		<!-------sidebar--design- close----------->
-
-
-
-		<!-------page-content start----------->
-
-		<div id="content">
-
-			<!------top-navbar-start----------->
-
-			<div class="top-navbar">
-				<div class="xd-topbar">
-					<div class="row">
-						<div class="col-2 col-md-1 col-lg-1 order-2 order-md-1 align-self-center">
-							<div class="xp-menubar">
-								<span class="material-icons text-white">signal_cellular_alt</span>
-							</div>
-						</div>
-
-						<div class="col-md-5 col-lg-3 order-3 order-md-2">
-							
-						</div>
-
-
-						<div class="col-10 col-md-6 col-lg-8 order-1 order-md-3">
-							<div class="xp-profilebar text-right">
-								<nav class="navbar p-0">
-									<ul class="nav navbar-nav flex-row ml-auto">
-										<li class="dropdown nav-item active">
-											<a class="nav-link" href="#" data-toggle="dropdown">
-												<span class="material-icons">notifications</span>
-												<span class="notification">4</span>
-											</a>
-											<ul class="dropdown-menu">
-												<li><a href="#">You Have 4 New Messages</a></li>
-												<li><a href="#">You Have 4 New Messages</a></li>
-												<li><a href="#">You Have 4 New Messages</a></li>
-												<li><a href="#">You Have 4 New Messages</a></li>
-											</ul>
-										</li>
-
-										<li class="nav-item">
-											<a class="nav-link" href="#">
-												<span class="material-icons">question_answer</span>
-											</a>
-										</li>
-
-										<li class="dropdown nav-item">
-											<a class="nav-link" href="#" data-toggle="dropdown">
-												<img src="profile2.avif" style="width:36px; height:35px; border-radius:45%;"/>
-												<span class="xp-user-live"></span>
-											</a>
-											<ul class="dropdown-menu small-menu">
-												<li><a href="#">
-														<span class="material-icons">person_outline</span>
-														Profile
-													</a></li>
-												<li><a href="#">
-														<span class="material-icons">settings</span>
-														Settings
-													</a></li>
-												<li><a href="logout.php">
-														<span class="material-icons">logout</span>
-														Logout
-													</a></li>
-
-											</ul>
-										</li>
-
-
-									</ul>
-								</nav>
-							</div>
-						</div>
-
-					</div>
-
-					<div class="xp-breadcrumbbar text-center">
-						<h4 class="page-title"> Approved Requests</h4>
-						<ol class="breadcrumb">
-
-						</ol>
-					</div>
-
-
-				</div>
-			</div>
+	
 			<!------top-navbar-end----------->
 
 
@@ -460,26 +265,10 @@ $user=$_SESSION['user'];
 
 
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="js/jquery-3.3.1.slim.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery-3.3.1.min.js"></script>
 
 
 	<script type="text/javascript">
-		$(document).ready(function () {
-			$(".xp-menubar").on('click', function () {
-				$("#sidebar").toggleClass('active');
-				$("#content").toggleClass('active');
-			});
-
-			$('.xp-menubar,.body-overlay').on('click', function () {
-				$("#sidebar,.body-overlay").toggleClass('show-nav');
-			});
-
-		});
+		
 
 		//select all and reject all
 		function selects() {
