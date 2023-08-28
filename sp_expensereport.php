769656ddcbf29e90702d5a6ef196a1d311c8744b
DeminKB <deminkbenny007@gmail.com>
2023-08-28 20:26:18 +0530

    added misc n xpense to sp

--- sp_expensereport.php
+++ sp_expensereport.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION['user']) or $_SESSION['user'] !== "super") {
     header('Location: login.php');
 }
 include 'sp_header.php';
+require 'connection.php';
 ?>
 <!doctype html>
 <html lang="en">
@@ -39,7 +40,214 @@ include 'sp_header.php';
     <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+		/* css for acceptAll and rejectAll Button*/
+		#acceptAllBtn {
+			color: rgb(229, 117, 56);
+			visibility: hidden;
+			margin-left: 0%;
+		}
 
+		#rejectAllBtn {
+			color: green;
+			visibility: hidden;
+		}
+		 /* Styling for the box container */
+		 .expensebox {
+      width: 400px;
+      height: 160px;
+      background-color:#cfcfc4 ;
+      border: 1px solid gray;
+      padding: 20px;
+	  box-shadow: 1px 2px 2px 2px rgba(20,20,20,0.4);
+	  margin-left:60px;
+	  flex:-1;
+	  margin-top:10px;
+      box-sizing: border-box;
+    }
+
+    /* Styling for the fields inside the box */
+    .expensefield {
+      display: inline-block;
+      margin-right: 20px;
+    }
+	table.table td:last-child{
+		   opacity:0.9;
+		   font-size:16px;
+		   margin:0px 5px;
+	   }
+
+    /* Style for the label element */
+    label {
+      display: inline-block;
+      
+    }
+
+    /* Style for the value element */
+    .expensevalue {
+      display: inline-block;
+    }
+	.bata-btn{
+		display: inline-block;
+  padding: 12px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  margin-left: 280px;
+  margin-top: 27px;
+  text-decoration: none;
+  border-radius: 4px;
+  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+  cursor: pointer;
+}
+.primary-button {
+  background-color:  #002147  ;
+  color: #ffffff;
+  border: 2px solid #002e63 ;
+}
+
+.primary-button:hover {
+  background-color: #0056b3;
+  border-color: #0056b3;
+}
+	.form-container {
+            display: flex;
+        }
+
+		.form {
+            flex: -2;
+           
+        }
+		.hidden-row {
+            display: none;
+        }
+		
+		table {
+  background: #152935;
+  border-radius: 0.25em;
+  border-collapse: collapse;
+  margin: 1em;
+  width: 1200px;
+  margin-left:30px;
+}
+th {
+  border-bottom: 1px solid #364043;
+  color: #da9100;
+  font-size: 0.85em;
+  font-weight: 600;
+  padding: 0.5em 1em;
+  text-align: left;
+}
+td {
+  color: #fff;
+  font-weight: 400;
+  padding: 0.65em 1em;
+}
+
+tbody tr {
+  transition: background 0.25s ease;
+}
+tbody tr:hover {
+  background: #014055;
+}
+.rowiee {
+	background-color: #436b95;
+}
+
+  .addnew {
+	display: inline-block;
+  padding: 8px 13px;
+  font-size: 12px;
+  
+  text-align: center;
+  margin-left:30px;
+  margin-top: 27px;
+  text-decoration: none;
+  border-radius: 4px;
+  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+  cursor: pointer;
+	 
+    }
+	.sec-button {
+		background-color:  #da9100  ;
+  color: #ffffff;
+  border: 2px solid #da9100 ;
+}
+.sec-button:hover {
+  background-color: #0056b3;
+  border-color: #0056b3;
+}
+	.tick-icon {
+      font-size: 15px;
+      color: #3cb371;
+	  width:28px;
+	  height:28px;
+	  background: #002147;
+    }
+	.delete-icon {
+    display: inline-block;
+    cursor: pointer;
+	font-size: 8px;
+  }
+  .delete-prompt {
+    display: none;
+	font-family: Arial, sans-serif;
+    position: fixed;
+    top: 57%;
+    left: 67%;
+	font: size 5px;
+	height:160px;
+	width: 270px;
+    transform: translate(-50%, -50%);
+    background-color: #e5e4e2 ;
+    border: 1px solid #ccc;
+    padding: 20px;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  }
+  .delete-prompt h2 {
+    margin-top: 0;
+  }
+  .btn-container {
+    text-align: center;
+    margin-top: 20px;
+  }
+  .btn {
+    padding: 8px 16px;
+    margin: 0 10px;
+    cursor: pointer;
+  }
+  .btn.delete {
+    background-color: #f44336;
+    color: white;
+  }
+  .btn.cancel {
+    background-color: #ccc;
+    color: black;
+  }
+  @media only screen and (max-width: 767px){
+	.expensebox {
+      width: 300px;
+      height: 160px;
+      background-color:#cfcfc4 ;
+      border: 1px solid gray;
+      padding: 20px;
+	  box-shadow: 1px 2px 2px 2px rgba(20,20,20,0.4);
+	  margin-left:5px;
+	  flex:-1;
+	  margin-top:10px;
+      box-sizing: border-box;
+    }
+	
+
+    /* Styling for the fields inside the box */
+    .expensefield {
+      display: inline-block;
+      margin-right: 20px;
+    }
+
+  }
+		
+	</style>
 </head>
 
 <body>
@@ -48,7 +256,171 @@ include 'sp_header.php';
 
     <!------main-content-start----------->
 
-    hii enter your contents here and remove this
+    <div class="main-content">
+				<div class="attendence" style="overflow-x:auto;">
+					
+					<table class="profile-box">
+							<thead>
+					            <tr>
+								<th>TITLE:</th>
+			                    <th>DATE:</th>
+	                            </tr>
+                    </table>
+					<table>
+						<th>
+                          <div class="expensebox">
+                            <div class="expensefield">
+                              <label for="opening-balance">Opening Balance: &emsp; </label>
+                              <span class="expensevalue">19215</span><br>
+	                          <label for="opening-balance">Advance: &emsp; &nbsp; &emsp; &emsp;  &emsp; </label>
+                              <span class="expensevalue">5000</span><br>
+	                          <label for="opening-balance">Total: &emsp; &emsp; &emsp; &emsp; &emsp;&emsp;&nbsp;</label>
+                              <span class="expensevalue">69215</span>
+                            </div>
+                          </div>
+                        </th>
+                        <th>
+				          <div class="expensebox" id="expensebox">
+                             <div class="expensefield">
+                                <label for="opening-balance">Location: &emsp; </label>
+                                <span class="expensevalue"> Diamond Plaza Trivanathapuram</span>
+	                          
+                             </div>
+                          </div>
+                        </th>
+                    </table>
+					
+					<br>
+					<button onclick="toggleRows()" class="addnew sec-button">ADD EXPENSE
+                      </button>
+
+		<div class="attendence" style="overflow-x:auto;">
+		
+				<table >
+										<?php
+// showing values in the table
+      $rowsql = "SELECT * FROM daily_expense";
+      $rowresult = mysqli_query($conn, $rowsql);
+      $sum=0;
+      $no=0;
+			if(mysqli_num_rows($rowresult)!=0){ 
+        ?>
+					<tr>
+                      <th>SI No.</th>
+                      <th>Name</th>
+                      <th>Purpose</th>
+                      <th>Amount</th>
+					            <th>Quantity</th>
+                       <th>Mode</th>
+
+                    </tr>
+					<?php
+while($row=mysqli_fetch_array($rowresult,MYSQLI_ASSOC)){
+  $time = new DateTime($row['datetime']);
+  $date = $time->format('j.n.Y');
+  $time = $time->format('H:i A');
+  $no=$no+1;
+  echo('
+
+                    <tr>
+                      <td>'.$no.'</td>
+                      <td>'.$row['name'].'</td>
+                      <td>'.$row['description'].'</td>
+                      <td>₹'.$row['price'].'</td>
+					  <td>'.$row['quantity'].'</td>
+					  <td></td>
+					  
+					  <td><div class="delete-icon" onclick="showDeletePrompt()">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M8 9v10c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V9H8zm14-4h-3.5l-1-1h-5l-1 1H2v2h20V5zm-4 11H6v-2h12v2z"/>
+    </svg>
+  </div>
+  </td>
+  <div class="delete-prompt" id="deletePrompt">
+    <i>Are you sure you want to delete this expense?</i>
+    <div class="btn-container">
+	 <form action="expensereport.php" method="post">
+	 <input type="text" name="id" value="'.$row['id'].'" hidden>
+      <button class="btn delete" type="submit" name="delete" onclick="deleteExpense()">Delete</button>
+      <button class="btn cancel" onclick="hideDeletePrompt()">Cancel</button>
+	  </form>
+    </div>
+  </div>
+
+					');
+					$sum=$sum+$row['price'];
+}
+      }
+   else{
+        echo('<h2>NO PENDTING REQUESTS</h2>');
+      }?>
+                    <tr class="hidden-row">
+						<form action="expensereport.php" method="post">
+					  <td></td>
+                      <td><input type="text" name="name" placeholder="Enter Name"></td>
+                      <td><input type="text" name="purpose" placeholder="Enter Purpose"></td>
+                      <td><input type="text" name="amount" placeholder="Enter Amount"></td>
+					  <td><input type="text" name="quantity" placeholder="Enter quantity"></td>
+					  <td></td>
+					  <td> <button class="tick-icon" name="inp" type="submit">
+						</form>
+    <i class="fas fa-check-circle"></i></td>
+                    </tr>
+					<tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+					  <td></td>
+                    </tr>
+					<tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+					  <td></td>
+                    </tr>
+					<tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+					  <td></td>
+                    </tr>
+					<tr class="rowiee">
+  
+                      <td ></td>
+                      <td>Closing Balance:</td>
+                      <td>$400</td>
+                      <td>Total Expense:</td>
+					  <td><?php echo('₹'.$sum.'');?></td>
+					  <td></td>
+					  <td ></td>
+                    </tr>
+					<tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+					  <td></td>
+                      <td></td>
+                    </tr>
+					
+                     
+                </table>
+
+                
+
+               
+
+                
+				<div>
+							<button id="acceptAllBtn" formaction="#">Accept All</button>
+							<button id="rejectAllBtn" formaction="#">Reject All</button>
+						</div><br>
+	
+				</div>
+			</div>
         <!------main-content-end----------->
 
 
@@ -68,7 +440,30 @@ include 'sp_header.php';
     <!-------complete html----------->
 
 
-   
+    <script type="text/javascript">
+	
+
+    //select all and reject all
+    function selects() {
+        var ele = document.getElementsByName("checkbox");
+        if (document.getElementById("selectAll").checked == true) {
+            document.getElementById("acceptAllBtn").style.visibility = "visible";
+            document.getElementById("rejectAllBtn").style.visibility = "visible";
+            for (var i = 0; i < ele.length; i++) {
+                if (ele[i].type == 'checkbox')
+                    ele[i].checked = true;
+            }
+        }
+        else{
+            document.getElementById("acceptAllBtn").style.visibility = "hidden";
+            document.getElementById("rejectAllBtn").style.visibility = "hidden";
+            for (var i = 0; i < ele.length; i++) {
+                if (ele[i].type == 'checkbox')
+                    ele[i].checked = false;
+            }
+        }
+    }
+</script>
 
 </body>
 
--- sp_misc.php
+++ sp_misc.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION['user']) or $_SESSION['user'] !== "super") {
     header('Location: login.php');
 }
 include 'sp_header.php';
+require 'connection.php';
 ?>
 <!doctype html>
 <html lang="en">
@@ -39,7 +40,148 @@ include 'sp_header.php';
     <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+  @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,600);
 
+.login-box {
+  position: absolute;
+  top: 17%;
+  left: 50%;
+  width: 40%;
+  height: 540px;
+  padding: 40px;
+  
+  transform: translate(-50%, -50%);
+  background: #152935;
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  border-radius: 10px;
+  margin-top: 200px;
+  
+}
+
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+}
+
+.login-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.login-box .user-box label {
+  position: absolute;
+  top:0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: .5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #03e9f4;
+  font-size: 12px;
+}
+
+.login-box form a input{
+  position: relative;
+  display: block;
+  padding: 10px 20px;
+  color: white;
+  background:black;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  margin-left: 140px;
+  margin-top: 20px;
+  margin-bottom:100px;
+  letter-spacing: 3px
+}
+
+#misc-submit{
+
+}
+
+table {
+  background: #152935;
+  border-radius: 0.25em;
+  border-collapse: collapse;
+  margin: 1em;
+  margin-top: 700px;
+  width: 95%;
+  margin-left:30px;
+}
+th {
+  border-bottom: 1px solid #364043;
+  color: #E2B842;
+  font-size: 0.85em;
+  font-weight: 600;
+  padding: 0.5em 1em;
+  text-align: left;
+}
+td {
+  color: #fff;
+  font-weight: 400;
+  padding: 0.65em 1em;
+}
+
+tbody tr {
+  transition: background 0.25s ease;
+}
+tbody tr:hover {
+  background: #014055;
+} 	
+.miscform-container {
+            display: flex;
+        }
+.bata-btn{
+		display: inline-block;
+  padding: 8px 8px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  margin-left: 400px;
+  margin-top: 27px;
+  text-decoration: none;
+  border-radius: 4px;
+  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+  cursor: pointer;
+  margin-bottom:20px;
+}
+.primary-button {
+  background-color:  #002147  ;
+  color: #ffffff;
+  border: 2px solid #002e63 ;
+}
+
+.primary-button:hover {
+  background-color: #0056b3;
+  border-color: #0056b3;
+}
+	/* css for acceptAll and rejectAll Button*/
+	.btnsCheck{
+		margin-left:3%;
+	}
+	
+    </style>
 </head>
 
 <body>
@@ -48,7 +190,122 @@ include 'sp_header.php';
 
     <!------main-content-start----------->
 
-    hii enter your contents here and remove this
+    <div class="login-box">
+  <h2>Enter the miscellaneous</h2>
+  <form action="" method="post">
+    <div class="user-box">
+      <input type="text" name="misc-name"  id = "misc-name" required>
+      <label>Name</label>  
+    </div>
+	<div class="user-box">
+      <input type="text" name="misc-purpose" id = "misc-purpose" required>
+      <label>Purpose</label>
+    </div>
+	<div class="user-box">
+      <input type="text" name="misc-amount" id = "misc-amount" required>
+      <label>Amount</label>
+    </div>
+	<div class="user-box">
+      <input type="text" name="misc-remark" id = "misc-remark"  required="false">
+      
+      <label>Remark</label>
+    </div>
+	<a href="#">
+	  <input type ="submit" name  = "misc-submit" id = "misc-submit" value = " Submit ">
+	</a>
+  </form>
+</div>
+
+<div class="misctable" style="overflow-x:auto;">
+<table>
+
+  <thead>
+                   <?php
+// showing values in the table
+      $rowsql = "SELECT id, timestamp, name, amount, purpose, remark FROM miscellaneous";
+      $rowresult = mysqli_query($conn, $rowsql);
+      $sum =0;
+      $no=0;
+			if(mysqli_num_rows($rowresult)!=0){ 
+        ?>
+    <tr>
+	<th><span class="custom-checkbox">
+											<input type="checkbox" onchange='selects()' id="selectAll">
+											<label for="selectAll"></label></th>
+      <th>SI NO</th>
+      <th>DATE</th>
+      <th> Name</th>
+      <th>PURPOSE</th>
+    
+      <th>TIME</th>
+      <th>REMARK</th>
+      <th>AMOUNT</th>
+     
+  </thead>
+  <tbody>
+
+		<?php
+while($row=mysqli_fetch_array($rowresult,MYSQLI_ASSOC)){
+  $time = new DateTime($row['timestamp']);
+  $date = $time->format('j.n.Y');
+  $time = $time->format('H:i A');
+  $no=$no+1;
+  echo('
+								<tr>
+								<td><span class="custom-checkbox">
+								<input type="checkbox" id="checkbox" name="checkbox" value="1">
+								<label for="checkbox1"></label></th>
+  <td>'.$no.'</td>
+      <td>'.$date.'</td>
+      <td>'.$row['name'].'</td>
+      <td>'.$row['purpose'].'</td>
+      <td>'.$time.'</td>
+      <td>'.$row['remark'].'</td>
+      <td>'.$row['amount'].'</td>');
+      
+      $sum = $sum + $row['amount'];
+      echo('</tr>');
+
+}
+ echo('      <tr>
+       
+      <td colspan="7" style="text-align:right;"> TOTAL:</td>
+	  
+      <td>'.$sum.'</td>
+</tr>');
+      }
+      
+   
+   
+
+ else{
+        echo('<h2>NO PENDTING REQUESTS</h2>');
+      }?>
+
+
+
+  </tbody>
+</table>
+<div class="btnsCheck">
+							<button id="acceptAllBtn" formaction="#">Accept All</button>
+							<button id="rejectAllBtn" formaction="#">Reject All</button>
+						</div>
+</div>
+          
+<br>
+<div class="miscform-container" id="miscbuttons">
+					<form form class="form" action="approved_requests.php" method="post">
+					    <input name="Approved Expense" type="submit" 
+						                    class="bata-btn primary-button" value="Send to Expense" id="aprovexpbtn">
+					  
+                    </form>	 
+					<form form class="form" action="misc.php" method="post">
+
+					    <input name="Miscellanious" type="submit"
+                                            class="bata-btn primary-button" value="Delete" id="submitbtn">
+					</form> 
+											
+</div>
         <!------main-content-end----------->
 
 
@@ -68,7 +325,30 @@ include 'sp_header.php';
     <!-------complete html----------->
 
 
-   
+    <script type="text/javascript">
+
+//Select all boxes of table
+
+function selects() {
+     var ele = document.getElementsByName("checkbox");
+     if (document.getElementById("selectAll").checked == true ) {
+         document.getElementById("acceptAllBtn").style.visibility = "visible";
+         document.getElementById("rejectAllBtn").style.visibility = "visible";
+         for (var i = 0; i < ele.length; i++) {
+             if (ele[i].type == 'checkbox')
+                 ele[i].checked = true;
+         }
+     }
+     else{
+         document.getElementById("acceptAllBtn").style.visibility = "hidden";
+         document.getElementById("rejectAllBtn").style.visibility = "hidden";
+         for (var i = 0; i < ele.length; i++) {
+             if (ele[i].type == 'checkbox')
+                 ele[i].checked = false;
+         }
+     }
+ }
+</script>
 
 </body>
 
