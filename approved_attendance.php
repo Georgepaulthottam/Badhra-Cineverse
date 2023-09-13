f640a21a74a9ac8add959e2f5d096787bdbe875f
DeminKB <deminkbenny007@gmail.com>
2023-09-13 19:26:07 +0530

    Update approved_attendance.php

--- approved_attendance.php
+++ approved_attendance.php
@@ -149,26 +149,9 @@ $result=mysqli_query($conn,$query);
 
 
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="js/jquery-3.3.1.slim.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery-3.3.1.min.js"></script>
+	<script>
 
-
-	<script type="text/javascript">
-		$(document).ready(function () {
-			$(".xp-menubar").on('click', function () {
-				$("#sidebar").toggleClass('active');
-				$("#content").toggleClass('active');
-			});
-
-			$('.xp-menubar,.body-overlay').on('click', function () {
-				$("#sidebar,.body-overlay").toggleClass('show-nav');
-			});
-
-		});
+	
 
 		//select all and reject all
 		function selects() {
